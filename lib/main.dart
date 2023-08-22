e9cfdfa5fef75b8f28e043b5e7153155dff7875e
Reydner96 <reyd2013@live.com>
2023-08-21 23:29:29 -0300

    Inicio do Projeto

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+main() {
+  runApp(new PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print('Pergunta respondida!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas.elementAt(0)),
+            const ElevatedButton(
+              onPressed: responder,
+              child: Text('Resposta 1'),
+            ),
+            const ElevatedButton(
+              onPressed: responder,
+              child: Text('Resposta 1'),
+            ),
+            const ElevatedButton(
+              onPressed: responder,
+              child: Text('Resposta 1'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
