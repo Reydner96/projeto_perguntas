26b6f1ccd26d5c82b7666afd3405dd974b297d96
Reydner96 {Reyd} <31974666+Reydner96@users.noreply.github.com>
2023-08-22 13:24:21 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,14 @@ main() {
   runApp(new PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget {
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
   void responder() {
-    print('Pergunta respondida!');
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print(perguntaSelecionada);
   }
 
   @override
@@ -24,22 +29,28 @@ class PerguntaApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text(perguntas.elementAt(0)),
+            Text(perguntas[perguntaSelecionada]),
             ElevatedButton(
               onPressed: responder,
               child: Text('Resposta 1'),
             ),
             ElevatedButton(
               onPressed: responder,
-              child: Text('Resposta 1'),
+              child: Text('Resposta 2'),
             ),
             ElevatedButton(
               onPressed: responder,
-              child: Text('Resposta 1'),
+              child: Text('Resposta 3'),
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
